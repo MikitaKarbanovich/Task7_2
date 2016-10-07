41a2a81f818992406f503568d44dd2565d8c9a38
MikitaKarbanovich <nikitakarbanovich@gmail.com>
2016-10-07 15:18:15 +0300

    Add project files.

---
+++ Task7_2/StringToInt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task7_2
+{
+    public class StringToInt
+    {
+        public string Input { get; set; }
+        public int ConvertStringToInt(string Input)
+        {
+            int outPut=1;
+            bool IsDigit = Input.Length == Input.Where(c => char.IsDigit(c)).Count();
+            if (IsDigit)
+            {
+                outPut = Convert.ToInt32(Input);
+            }
+            return outPut;
+        }
+    }
+}
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Task7_2;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTests
+    {
+        StringToInt stringToInt;
+        [TestInitialize]
+        public void Initialize()
+        {
+            stringToInt = new StringToInt();
+        }
+
+        [TestCleanup()]
+        public void Cleanup()
+        {
+            stringToInt = null;
+        }
+
+        [TestMethod]
+        public void TesetCustomersWithSumOfOrdersMoreThan()
+        {
+            stringToInt.Input = "100500";
+            Assert.AreEqual(100500, stringToInt.Input);
+        }
+       
+    }
+}
